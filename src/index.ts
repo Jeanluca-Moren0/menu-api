9afa6046e94d7c0d208d1ac5a9d4acb06f79e98b
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-01-29 23:31:05 -0300

    Project initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import { itemsRouter } from './items/items.router'
+import { errorHandler } from './middleware/error.middleware'
+import { notFoundHandler } from './middleware/not-found.middleware'
+
+dotenv.config()
+
+
+if(!process.env.PORT){
+  process.exit(1)
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use("/api/menu/items", itemsRouter)
+
+app.use(errorHandler)
+app.use(notFoundHandler)
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta a ${PORT}`)
+})
